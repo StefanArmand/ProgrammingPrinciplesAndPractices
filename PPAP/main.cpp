acfab333d23dd6c601709714ee9d67813c2f282c
Stefan <stefan_armand@yahoo.com>
2018-02-22 16:12:25 +0200

    exercise 2 chapter 3 done

--- PPAP/main.cpp
+++ PPAP/main.cpp
@@ -5,4 +5,9 @@ using namespace std;
 
 int main()
 {
+	int distance = 0;
+	cout << "Write the distance in miles to be converted in km\n";
+	cin >> distance;
+	cout << "the distance in km is " << distance * 1.609 << "\n";
+	keep_window_open();
 }
