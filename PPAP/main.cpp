3213ff69622567edacba6763882458f7432c6011
Stefan <stefan_armand@yahoo.com>
2018-02-22 16:06:57 +0200

    Glossary chapter 3 done

--- PPAP/main.cpp
+++ PPAP/main.cpp
@@ -5,4 +5,44 @@ using namespace std;
 
 int main()
 {
+	cout << "Terms: \n";
+	cout << "// = token that signals comment break \n";
+	cout << "<< = output operator \n";
+	cout << "C++ = the compiled programming language \n";
+	cout << "comment = a text that does not get compiled, that is marked with // \n";
+	cout << "compiler = a program that turn writen text into commands that the computer can understand \n";
+	cout << "compile time error = error found by the compiler \n";
+	cout << "cout = character output stream \n";
+	cout << "executable = file that can be run when selected \n";
+	cout << "function = a set of instructions \n";
+	cout << "header = a file that contains definitions of terms\n";
+	cout << "IDE = integrated development environment \n";
+	cout << "#include = a directive that adds a source file into another \n";
+	cout << "library = code written by others we access using #include \n";
+	cout << "linker = parts we write combined with parts from the C++ standard library \n";
+	cout << "main() = the main function\n";
+	cout << "object code = a region of memory with a type that specifies what kind of information can be placed in it \n";
+	cout << "statement = action to be performed\n";
+	cout << "assignment = the = operator, that gives a variable a new value\n";
+	cout << "cin = the command that takes in what the user writes\n";
+	cout << "concatenation = adding text at the end of a string \n";
+	cout << "conversion = turing a type of format from one to another, from int to double \n";
+	cout << "declaration = when you give a name to an object \n";
+	cout << "decrement = subtract \n";
+	cout << "definition = statement that introduces a new name and sets aside memory for a variable \n";
+	cout << "increment = add \n";
+	cout << "initialization = the starting value \n";
+	cout << "name = the name of the variable \n";
+	cout << "narrowing = putting a value into an object that may be too small \n";
+	cout << "object = a region of memory with a type that specifies what kind of infomration can be placed in it \n";
+	cout << "operation = a set of instruction the user wants \n";
+	cout << "operator = like '=' '||' and so on \n";
+	cout << "type = the type of variable in memory, like int, char, bool \n";
+	cout << "type safety = using the type as its meant to be used \n";
+	cout << "value = what is the value of a variable \n";
+	cout << "variable = an object that is named and has memory assigned to it \n";
+	cout << " \n";
+	cout << " \n";
+	keep_window_open();
+	return 0;
 }
