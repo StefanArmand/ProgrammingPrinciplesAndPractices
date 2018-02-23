bc9b8f2ac4adc64ed78c3aebf000304552bf5ae4
Stefan <stefan_armand@yahoo.com>
2018-02-23 11:18:31 +0200

    Ch3Ex9 done

--- PPAP/main.cpp
+++ PPAP/main.cpp
@@ -5,4 +5,14 @@ using namespace std;
 
 int main()
 {
+	string value = " ";
+	cout << "Write a number in string form\n";
+	cin >> value;
+	if (value == "zero") { cout << "0\n"; }
+	else if (value == "one") { cout << "1\n"; }
+	else if (value == "two") { cout << "2\n"; }
+	else if (value == "three") { cout << "3\n"; }
+	else if (value == "four") { cout << "4\n"; }
+	else { cout << "No idea what number is that\n"; }
+	keep_window_open();
 }
