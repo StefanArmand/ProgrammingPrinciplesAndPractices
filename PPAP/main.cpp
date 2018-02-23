66a9328f49e44d463ae3b9872598edb502d5890e
Stefan <stefan_armand@yahoo.com>
2018-02-23 11:36:26 +0200

    Ch3Ex10 done

--- PPAP/main.cpp
+++ PPAP/main.cpp
@@ -5,4 +5,22 @@ using namespace std;
 
 int main()
 {
+	string operation{ " " };
+	double val1 = 0;
+	double val2 = 0;
+	cout << "Write the operation you want as +, -, *, /, plus, minus, mul, div\n";
+	cin >> operation;
+	cout << "Enter the first operand\n";
+	cin >> val1;
+	cout << "Enter the second operand\n";
+	cin >> val2;
+	if (operation == "+"){ cout << val1 << " + " << val2 << " = " << val1 + val2 << "\n";}
+	else if (operation == "-") { cout << val1 << " - " << val2 << " = " << val1 - val2 << "\n"; }
+	else if (operation == "*") { cout << val1 << " * " << val2 << " = " << val1 * val2 << "\n"; }
+	else if (operation == "/") { cout << val1 << " / " << val2 << " = " << val1 / val2 << "\n"; }
+	else if (operation == "plus") { cout << val1 << " + " << val2 << " = " << val1 + val2 << "\n"; }
+	else if (operation == "minus") { cout << val1 << " - " << val2 << " = " << val1 - val2 << "\n"; }
+	else if (operation == "mul") { cout << val1 << " * " << val2 << " = " << val1 * val2 << "\n"; }
+	else if (operation == "div") { cout << val1 << " / " << val2 << " = " << val1 / val2 << "\n"; }
+	keep_window_open();
 }
