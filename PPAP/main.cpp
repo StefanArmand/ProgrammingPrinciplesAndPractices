04291d53db4175245be46cc14e08236e4fab95ea
Stefan <stefan_armand@yahoo.com>
2018-02-23 11:06:24 +0200

    Ch3Ex8 done

--- PPAP/main.cpp
+++ PPAP/main.cpp
@@ -5,4 +5,10 @@ using namespace std;
 
 int main()
 {
+	int value = 0;
+	cout << "Please enter the number\n";
+	cin >> value;
+	if (value % 2 == 0) { cout << value << " is even\n"; }
+	else { cout << value << " is odd\n"; }
+	keep_window_open();
 }
