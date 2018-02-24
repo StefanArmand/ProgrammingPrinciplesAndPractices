69689f47fc220685e432836b031aecf5aa12c9bd
Stefan <stefan_armand@yahoo.com>
2018-02-24 14:07:21 +0200

    Ch4Try1 done

--- PPAP/main.cpp
+++ PPAP/main.cpp
@@ -5,4 +5,16 @@ using namespace std;
 
 int main()
 {
+	constexpr  double econversion = 1.22;
+	constexpr  double yconversion = 0.009;
+	constexpr  double gconversion = 1.39;
+	double value = 1;
+	string type { " " };
+	cout << "Please enter an ammount followed by its type (yen, pounds, euro):\n";
+	cin >> value >> type;
+	if (type == "yen") { cout << value << " yen = " << value * yconversion << " dollars\n"; }
+	else if (type == "pounds") { cout << value << " pounds = " << value * gconversion << " dollars\n"; }
+	else if (type == "euro") { cout << value << " euro = " << value * econversion << " dollars\n"; }
+	else { cout << "I don't know that currency\n"; }
+	keep_window_open();
 }
