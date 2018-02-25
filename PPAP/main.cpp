548d1f48356c42cb66cca4831cf0901acfc3968e
Stefan <stefan_armand@yahoo.com>
2018-02-25 18:36:41 +0200

    Ch4Try5 Square with sum done

--- PPAP/main.cpp
+++ PPAP/main.cpp
@@ -3,6 +3,17 @@
 
 using namespace std;
 
+int value = 0;
+
+int square (int x)
+{
+	int s{ 0 };
+	for (auto i = 0; i < x; i++) { s += x; }
+	return s;
+}
+
 int main()
 {
+	cout << square(5) << "\n";
+	keep_window_open();
 }
