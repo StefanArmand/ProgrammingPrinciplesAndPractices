35d979f3ba1fa5261948a88be8cb1990b0927a54
Stefan <stefan_armand@yahoo.com>
2018-02-25 15:02:55 +0200

    Ch4Try4 done

--- PPAP/main.cpp
+++ PPAP/main.cpp
@@ -5,4 +5,15 @@ using namespace std;
 
 int main()
 {
+	char letter = 'a';
+	int number = 96;
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		cout << letter << " " << ++number << "\t" << "\n";
+	}
+	for (letter = 'A'; letter <= 'Z'; letter++)
+	{
+		cout << letter << " " << ++number << "\t" << "\n";
+	}
+	keep_window_open();
 }
