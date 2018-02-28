de3408d8b9a9997dbe3d83b484fa379098298b46
Stefan <stefan_armand@yahoo.com>
2018-02-28 10:12:46 +0200

    Ch4Drill somewhat done

--- PPAP/main.cpp
+++ PPAP/main.cpp
@@ -5,4 +5,56 @@ using namespace std;
 
 int main()
 {
+	double value = 0;
+	double lowest = 0;
+	double highest = 0;
+	string unit = " ";
+	double sum = 0;
+	vector <double> list;
+	
+
+	cout << "Please enter a number with a unit type (cm, m, in, ft): ";
+	while (cin >> value >> unit)
+	{
+		if (unit == "m") { value = value * 100; }
+		else if (unit == "in") { value = value * 2.5; }
+		else if (unit == "ft") { value = value * 12 * 2.5; }
+		else if (unit == "cm") { value = value; }
+		else { cout << "Unknown unit\n" << endl; }
+		if (lowest == 0 && highest ==0 )
+		{
+			lowest = value;
+			highest = value;
+			list.push_back(value * 100);
+			cout << "The value " << lowest << " cm is the smallest so far\n";
+			cout << "The value " << highest << " cm is highest so far\n";
+		}
+		else if (value < lowest)
+		{
+			lowest = value;
+			list.push_back(value * 100);
+			cout << "The value " << lowest << " cm is the smallest so far\n";
+			cout << "The value " << highest << " cm is still highest so far\n";
+		}
+		else if (value > highest)
+		{
+			highest = value;
+			list.push_back(value * 100);
+			cout << "The value " << lowest << " cm is still the smallest so far\n";
+			cout << "The value " << highest << " cm is highest so far\n";
+		}
+		else
+		{
+			list.push_back(value * 100);
+			cout << "The value " << lowest << " cm is still the smallest so far\n";
+			cout << "The value " << highest << " cm is still the highest so far\n";
+		}
+		
+		sum += value;
+		cout << "The sum of the values written so far is " << sum/100 << " m\n";		
+	}
+	sort(list.begin(), list.end());
+	for (int i = 0; i<list.size(); ++i)
+	cout << list[i] << " m\n";
+	keep_window_open();
 }
