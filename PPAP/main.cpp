e2480f8a2b5ce8ef5cfc1b5378d59684b47fcd7a
Stefan <stefan_armand@yahoo.com>
2018-02-28 19:30:58 +0200

    Ch4Ex3 wip

--- PPAP/main.cpp
+++ PPAP/main.cpp
@@ -5,4 +5,45 @@ using namespace std;
 
 int main()
 {
+	vector <double> distances;
+	double distance = 0;
+	double total = 0;
+	double smallest = 0;
+	double greatest = 0;
+	double mean = 0;
+
+	while (cin >> distance)
+	{
+		if (smallest == 0 && greatest == 0)
+		{
+			smallest = distance;
+			greatest = distance;
+			distances.push_back(distance * 100);
+			cout << "The distance " << smallest << " m is the smallest so far\n";
+			cout << "The distance " << greatest << " m is greatest so far\n";
+		}
+		else if (distance < smallest)
+		{
+			smallest = distance;
+			distances.push_back(distance * 100);
+			cout << "The distance " << smallest << " m is the smallest so far\n";
+			cout << "The distance " << greatest << " m is still greatest so far\n";
+		}
+		else if (distance > greatest)
+		{
+			greatest = distance;
+			distances.push_back(distance * 100);
+			cout << "The distance " << smallest << " m is still the smallest so far\n";
+			cout << "The distance " << greatest << " m is greatest so far\n";
+		}
+		else
+		{
+			distances.push_back(distance * 100);
+			cout << "The distance " << smallest << " m is still the smallest so far\n";
+			cout << "The distance " << greatest << " m is still the greatest so far\n";
+		}
+
+		total += distance;
+		cout << "The sum of the distances written so far is " << total << " m\n";
+	}
 }
