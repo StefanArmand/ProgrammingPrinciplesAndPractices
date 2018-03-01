7d751064b2d1904a4a9a48c09a35eb737d009174
Stefan <stefan_armand@yahoo.com>
2018-03-01 10:34:31 +0200

    Ch4Ex2 done

--- PPAP/main.cpp
+++ PPAP/main.cpp
@@ -6,19 +6,26 @@ using namespace std;
 int main()
 {
 	vector <double> temps;
-	for (double temp; cin >> temp;)
-		temps.push_back(temp);
-	double sum = 0;
-	for (int x : temps) sum += x;
+	for (double temp; cin >> temp;) {
+		if (temp != 55) {
+			temps.push_back(temp);
+		}
+		else {
+			double sum = 0;
+			for (auto x : temps) sum += x;
+
+			//sort(temps);
+			sort(temps.begin(), temps.end());
+			double median{ 0 };
+			if (temps.size() % 2 == 0)
+				median = temps[(temps.size() - 1) / 2] + temps[temps.size() / 2];
+			else
+				median = temps[temps.size() / 2];
+			//
+			cout << " Median temperature: " << median << "\n";
+		}
+	}
 	
-	//sort(temps);
-	sort(temps.begin(), temps.end());
-	double median{ 0 };
-	if (temps.size() % 2 == 0)
-		median = temps[(temps.size() -1) / 2] + temps[temps.size() / 2];
-	else
-		median= temps[temps.size() / 2];
-	//keep_window_open();
-	cout << " Median temperature: " << median << "\n";
+	keep_window_open();
 	return 0;
 }
