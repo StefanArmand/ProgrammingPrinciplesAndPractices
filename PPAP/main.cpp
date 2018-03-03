80ea3c1b2d7f2505c7a998a50bec939fb59cdeaf
Stefan <stefan_armand@yahoo.com>
2018-03-03 17:22:47 +0200

    Ch4Ex7 Done

--- PPAP/main.cpp
+++ PPAP/main.cpp
@@ -3,17 +3,56 @@
 
 using namespace std;
 
+vector<string> numbers;
+void initialize_numbers()
+{
+	numbers.push_back("zero");
+	numbers.push_back("one");
+	numbers.push_back("two");
+	numbers.push_back("three");
+	numbers.push_back("four");
+	numbers.push_back("five");
+	numbers.push_back("six");
+	numbers.push_back("seven");
+	numbers.push_back("eight");
+	numbers.push_back("nine");
+}
+
+int get_number()
+{
+	int number;
+	string name;
+	bool repeat = false;
+	int result{ -1 };
+	if (cin >> number)
+	{
+		return number;
+	}
+	cin.clear();
+	cin >> name;
+	for (size_t i = 0; i < numbers.size(); ++i) {
+		if (numbers[i] == name) {
+			result = i;
+		}
+	}
+	if (result == -1) {
+		cout << "The name <" << name << "> does not correspond to any single digit number.\n";
+	}
+	return result;
+}
+
+
+char operation = ' ';
+
 int main()
 {
-	double val1 = 0;
-	double val2 = 0;
-	char operation = ' ';
 
-	cout << "Please write the first value" << "\n";
-	cin >> val1;
-	cout << "Please write the second value" << "\n";
-	cin >> val2;
+	initialize_numbers();
+	cout << "Please enter a number from 0 to 9 two times" << "\n";
 	cout << "Please write the operation (+, -, *, /)" << "\n";
+	cout << "Please enter | to exit" << "\n";
+	int val1 = get_number();
+	int val2 = get_number();
 	cin >> operation;
 
 	if (operation == '+')
@@ -38,3 +77,10 @@ int main()
 	}
 	keep_window_open();
 }
+
+
+
+
+
+
+
