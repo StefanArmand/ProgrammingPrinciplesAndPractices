583d7d256e1665aa6d0e377533301e8765c4e66e
Stefan <stefan_armand@yahoo.com>
2018-03-04 12:12:21 +0200

    Ch4Ex9 done

--- PPAP/main.cpp
+++ PPAP/main.cpp
@@ -3,6 +3,23 @@
 
 using namespace std;
 
+// calculate number of rice grains the inventor asked for (64 squares). What is the largest number of squares for exact number with int and aproximate with double
+
 int main()
 {
+	double square = 64;
+	double grains = 1;
+	double totalsq = 0;
+
+	for (auto i=0; i<=square; i++)
+	{
+		while (grains > 0) {
+			grains *= 2;
+			cout << grains << "\n";
+			++totalsq;
+		}
+		cout << totalsq << "\n";
+	}
+
+	keep_window_open();
 }
