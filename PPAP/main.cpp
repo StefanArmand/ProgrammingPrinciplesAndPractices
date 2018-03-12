4cee5c3ca3adcdb355bc0ed15937bfbbf1a7892f
Stefan <stefan_armand@yahoo.com>
2018-03-12 19:02:40 +0200

    Ch5Ex7 done

--- PPAP/main.cpp
+++ PPAP/main.cpp
@@ -3,6 +3,32 @@
 
 using namespace std;
 
-int main()
+auto a = 3;
+auto b = 2;
+auto c = 5;
+auto r = 0;
+
+int insidesqr(int r)
 {
+
+	r = b * b - 4 * a*c;
+	if (r<0)
+	{
+		cout << "sqrt is invalid\n";
+		return 1;
+	}
+	return r;
+}
+
+int main()
+{	
+	double res1 = 0;
+	double res2 = 0;
+
+	res1 = (-b + sqrt(insidesqr(r))) / (2 * a);
+	res2 = (-b - sqrt(insidesqr(r))) / (2 * a);
+
+	cout << "Result 1 is " << res1<< "\n";
+	cout << "Result 2 is " << res2 << "\n";
+	keep_window_open();
 }
