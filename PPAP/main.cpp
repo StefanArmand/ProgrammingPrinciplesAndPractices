98bc050813de49ff2e69d1c256260129e3c1819e
Stefan <stefan_armand@yahoo.com>
2018-03-12 18:16:54 +0200

    Ch5Ex6 Done

--- PPAP/main.cpp
+++ PPAP/main.cpp
@@ -3,6 +3,55 @@
 
 using namespace std;
 
+//Convert Celsius to Fahrenheit and back
+
+double ctof(double c)
+{
+	double f = (c*1.8) + 32;
+	if (f<-460)
+	{
+		cout << "The temperature is too low\n";
+	}
+	else
+	{
+		return f;
+	}
+}
+double ftoc(double f)
+{
+	double c = (f - 32) / 1.8;
+	if (c < -273)
+	{
+		cout << "The temperature is too low\n";
+	}
+	else
+	{
+		return c;
+	}
+}
+
 int main()
 {
+	double d = 0;
+	cout << "Please enter a temperature:\n";
+	cin >> d;
+	cout << "Please enter the unit, Celsius or Fahrenheit (c or f)\n";
+	char unit = ' ';
+	cin >> unit;
+	if (unit == 'c')
+	{
+		double f = ctof(d);
+		cout << "The temperature in Fahrenheit is " << f << "\n";
+	}
+	else if (unit == 'f')
+	{
+		double c = ftoc(d);
+		cout << "The temperature in Celsius is " << c << "\n";
+	}
+	else
+	{
+		cout << "The temperature is unknown\n";
+	}
+
+	keep_window_open();
 }
