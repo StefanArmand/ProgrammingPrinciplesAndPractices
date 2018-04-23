f341c7e4ec6a3c58155c8f34b36d01a6c19e7036
Stefan <stefan_armand@yahoo.com>
2018-04-23 11:10:41 +0300

    Ch8Ex10 done

--- PPAP/main.cpp
+++ PPAP/main.cpp
@@ -3,6 +3,21 @@
 
 using namespace std;
 
+int maxv(vector<int> v) {
+	int temp = 0;
+	int max = 0;
+	for (int i = 0; i < v.size(); ++i) {
+		temp = v[i];
+		if (temp > max) {
+			max = temp;
+		}
+	}
+	return max;
+}
+
 int main()
 {
+	vector<int> v1{ 5, 21, 33, 1, 52 };
+	cout << "The largest number is: " << maxv(v1) << "\n";
+	keep_window_open();
 }
